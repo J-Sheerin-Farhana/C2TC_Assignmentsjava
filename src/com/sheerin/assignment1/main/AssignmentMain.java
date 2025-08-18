37d37e23b7c3f1e808530bdad43fa0e8607af81b
SHEERIN FARHANA <SHEERIN FARHANA@LAPTOP-VR8H9ER8>
2025-08-18 15:56:50 +0530

    To demonstrate understanding of Java packages and access modifiers

---
+++ src/com/sheerin/assignment1/main/AssignmentMain.java
@@ -0,0 +1,25 @@
+package com.sheerin.assignment1.main;
+
+import com.sheerin.assignment1.employees.Manager;
+import com.sheerin.assignment1.employees.Developer;
+import com.sheerin.assignment1.utilities.EmployeeUtilities;
+
+  //Main class to test Employee, Manager, Developer and Utilities.
+
+public class AssignmentMain {
+    public static void main(String[] args) {
+        Manager mgr = new Manager("Alice", "M001", 80000, "IT");
+        Developer dev = new Developer("Bob", "D001", 60000, "Java");
+
+        EmployeeUtilities util = new EmployeeUtilities();
+
+        util.printEmployeeDetails(mgr);
+        util.printEmployeeDetails(dev);
+
+        util.increaseSalary(mgr, 10);
+        util.increaseSalary(dev, 15);
+
+        util.printEmployeeDetails(mgr);
+        util.printEmployeeDetails(dev);
+    }
+}
