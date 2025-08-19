c515bde0c38f4eecf7a146d7a4d4f6a7935c4d1f
SHEERIN FARHANA <SHEERIN FARHANA@LAPTOP-VR8H9ER8>
2025-08-19 22:40:14 +0530

    This is my 3rd Java Assignment (Library)

---
+++ src/com/sheerin/assignment3/Book.java
@@ -0,0 +1,18 @@
+package com.sheerin.assignment3;
+
+//Subclass Book
+class Book extends Item {
+ // Additional attribute author
+ String author;
+
+ Book(int id, String title, String author) {
+	 //invoke Item class constructor
+     super(id, title);
+     this.author = author;
+ }
+
+ @Override //override Item class displayInfo() method
+ void displayInfo() {
+     System.out.println("Book: [ID: " + id + ", Title: " + title + ", Author: " + author + "]");
+ }
+}
